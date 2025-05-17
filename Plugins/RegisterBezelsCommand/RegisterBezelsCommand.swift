77b942d7e976d7c09b0a339e05fd8b217d3d5539
shitamori1272 <shitamori1272@gmail.com>
2025-05-17 13:29:55 +0900

    Add warning and dummy file registration when bezels are missing

--- Plugins/RegisterBezelsCommand/RegisterBezelsCommand.swift
+++ Plugins/RegisterBezelsCommand/RegisterBezelsCommand.swift
@@ -11,10 +11,30 @@ import PackagePlugin
 @main
 struct RegisterBezelsCommand: BuildToolPlugin {
     func createBuildCommands(context: PackagePlugin.PluginContext, target: any PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
-
         let cacheDirectoryURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
         let bezelsDirectoryURL = cacheDirectoryURL.appending(path: "com.shitamori1272.AppScreenshotKit/AppleDesignResource")
         let outputDirectoryURL = context.pluginWorkDirectoryURL.appending(path: "AppleDesignResource")
+
+        guard FileManager.default.fileExists(atPath: bezelsDirectoryURL.path) else {
+            Diagnostics.warning("No bezels found in \(bezelsDirectoryURL.path). Please run the command to download bezels first.\n \"swift run AppScreenshotKitCLI download-bezel-image\"")
+
+            return [
+                .buildCommand(
+                    displayName: "Register Dummy Bezel image file",
+                    executable: URL(fileURLWithPath: "/usr/bin/touch"),
+                    arguments: [
+                        outputDirectoryURL.appending(path: "dummy.txt").path()
+                    ],
+                    environment: [:],
+                    inputFiles: [
+                        cacheDirectoryURL
+                    ],
+                    outputFiles: [
+                        outputDirectoryURL
+                    ]
+                )
+            ]
+        }
 
         return [
             .buildCommand(
