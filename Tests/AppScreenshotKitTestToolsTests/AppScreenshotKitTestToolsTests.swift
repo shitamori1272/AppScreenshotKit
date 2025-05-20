98d3ea69abcbfd8035b41dda66614c4456c752a5
shitamori1272 <shitamori1272@gmail.com>
2025-05-20 15:18:26 +0900

    Refactor AppScreenshotKitTestToolsTests for improved clarity and consistency; remove commented-out code and ensure proper import statements.

--- Tests/AppScreenshotKitTestToolsTests/AppScreenshotKitTestToolsTests.swift
+++ Tests/AppScreenshotKitTestToolsTests/AppScreenshotKitTestToolsTests.swift
@@ -1,30 +1,38 @@
-// //
-// //  AppScreenshotKitTestToolsTests.swift
-// //  AppScreenshotKit
-// //
-// //  Created by Shuhei Shitamori on 2025/05/11.
-// //
+//
+//  AppScreenshotKitTestToolsTests.swift
+//  AppScreenshotKit
+//
+//  Created by Shuhei Shitamori on 2025/05/11.
+//
 
-// import XCTest
-// import AppScreenshotKit
-// @testable import AppScreenshotKitTestTools
+import XCTest
+import AppScreenshotKit
+import SwiftUI
+@testable import AppScreenshotKitTestTools
 
-// class AppScreenshotKitTestToolsTests: XCTestCase {
-    
-//     @MainActor
-//     func testExportAttachments() throws {
-//         let exporter = AppScreenshotExporter(option: .attachment(xcTestCase: self))
-//         try exporter.export(AppScreenshoSamples.AppScreenshotIPad.self)
-//     }
-    
-//     @MainActor
-//     func testExportPNGFiles() throws {
-//         let exporter = AppScreenshotExporter(
-//             option: .file(
-//                 try AppScreenshotKitUtils.packageURL().appendingPathComponent("Screenshots")
-//             )
-//         )
-        
-//         try exporter.export(AppScreenshoSamples.AppScreenshotKitREADME.self)
-//     }
-// }
+class AppScreenshotKitTestToolsTests: XCTestCase {
+
+    @MainActor
+    func testExportAttachments() throws {
+        let exporter = AppScreenshotExporter(option: .attachment(testCase: self))
+        try exporter.export(TestScreenshot.self)
+    }
+
+    @MainActor
+    func testExportPNGFiles() throws {
+        let outputURL = FileManager.default.temporaryDirectory.appending(path: "ScreenshotOutput")
+        let exporter = AppScreenshotExporter(option: .file(outputURL: outputURL))
+        try exporter.export(TestScreenshot.self)
+
+        let contents = try XCTUnwrap(FileManager.default.subpaths(atPath: outputURL.path()))
+        XCTAssertTrue(contents.contains(where: { $0.hasSuffix("TestScreenshot.png") }))
+    }
+}
+
+@AppScreenshot
+struct TestScreenshot: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
